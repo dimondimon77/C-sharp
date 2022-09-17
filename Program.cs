746bf44a53235f0fe35bc9af74272684bb093c69
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-17 13:39:16 +0300

    Решенная задача 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿Console.WriteLine("Введите число num1 : = " );
+Console.WriteLine("Введите число num2 : = " );
+Console.WriteLine("Введите число num3 : = " );
+Console.WriteLine("Введите число num4 : = " );
+Console.WriteLine("Введите число num5 : = " );
+Console.WriteLine("Введите число num6 : = " );
+int num1 = Convert.ToInt16(Console.ReadLine());
+int num2 = Convert.ToInt16(Console.ReadLine());
+int num3 = Convert.ToInt16(Console.ReadLine());
+int num4 = Convert.ToInt16(Console.ReadLine());
+int num5 = Convert.ToInt16(Console.ReadLine());
+int num6 = Convert.ToInt16(Console.ReadLine());
+if(num1>num2)
+Console.WriteLine("Максимальное число: =" + num1);
+else
+Console.WriteLine("Максимальное число: =" + num2);
+if(num3>num4)
+Console.WriteLine("Максимальное число: =" + num3);
+else
+Console.WriteLine("Максимальное число: =" + num4);
+if(num5>num6)
+Console.WriteLine("Максимальное число: =" + num5);
+else
+Console.WriteLine("Максимальное число: =" + num6);
+
+
