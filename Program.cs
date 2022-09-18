79984cf16bc5ead337f22576ee82a75a063eb16d
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-18 11:34:18 +0300

    С использовнием массива

--- Program.cs
+++ Program.cs
@@ -4,18 +4,19 @@ Console.WriteLine("Введите число num3 : = " );
 int num1 = Convert.ToInt16(Console.ReadLine());
 int num2 = Convert.ToInt16(Console.ReadLine());
 int num3 = Convert.ToInt16(Console.ReadLine());
+int[] nums = {num1,num2,num3};
+int index = 0;
 int max = 0;
-if (num1>num2)
+for (;index < nums.Length;index++)
 {
-max = num1;
-}
-else
-{
-max = num2;
-}
-if (num3>num2)
-{
-max = num3;
+    if(nums[0]>nums[1]){
+        max = nums[0];
+    }
+    if(max<nums[index])
+    {
+        max = nums[index];
+    }
+        
 }
 Console.WriteLine("Максисмальное число =" + max);
 
