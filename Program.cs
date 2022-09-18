33820b9cead3fc6305b1e41da4772692536889d8
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-18 11:50:42 +0300

    Решенная с массивом рабочая

--- Program.cs
+++ Program.cs
@@ -1,23 +1,23 @@
 Console.WriteLine("Введите число num1 : = " );
 Console.WriteLine("Введите число num2 : = " );
 Console.WriteLine("Введите число num3 : = " );
+Console.WriteLine("Введите число num4 : = " );
+Console.WriteLine("Введите число num5 : = " );
 int num1 = Convert.ToInt16(Console.ReadLine());
 int num2 = Convert.ToInt16(Console.ReadLine());
 int num3 = Convert.ToInt16(Console.ReadLine());
-int[] nums = {num1,num2,num3};
+int num4 = Convert.ToInt16(Console.ReadLine());
+int num5 = Convert.ToInt16(Console.ReadLine());
+int[] nums = {num1,num2,num3,num4,num5};
 int index = 0;
 int max = 0;
 for (;index < nums.Length;index++)
 {
-    if(nums[0]>nums[1]){
-        max = nums[0];
-    }
     if(max<nums[index])
     {
         max = nums[index];
     }
-        
-}
+      }
 Console.WriteLine("Максисмальное число =" + max);
 
 
