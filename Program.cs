db0b982b9449c5a25b206f925a99519aaba24400
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-19 21:34:22 +0300

    Полностю решена

--- Program.cs
+++ Program.cs
@@ -1,17 +1,9 @@
-Console.WriteLine("Введите число number : = " );
-int number = Convert.ToInt16(Console.ReadLine());
-int num1 = 7;
-int num2 = 20;
-Boolean comparison1 = true;
-Boolean comparison2 = true;
-comparison1 = number%num1 == 0;
-comparison2 = number%num2 == 0;
-
-if(comparison1&comparison2)
+Console.WriteLine("Введите число 1 : = " );
+Console.WriteLine("Введите число 2 : = " );
+int num1 = Convert.ToInt16(Console.ReadLine());
+int num2 = Convert.ToInt16(Console.ReadLine());
+int result = num1 * num1;
+if(result == num2)
 {
     Console.WriteLine("Успех");
 }
-//if(!briefly)
-//{
-    //Console.WriteLine("Второе число не кратно первому");
-//}
