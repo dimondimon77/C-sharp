ce2368f7b7fd217232f34f23b251a200d63eb4a0
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-19 22:25:27 +0300

    Готовая рандомное заполнение массива

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-
-int x = new Random(). Next(1,1000);
-Console.WriteLine(x);
+Console.WriteLine("Введите число");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] array= new int[lenght];
+for (int i = 0;i<lenght;i++)
+{
+    int x = new Random(). Next(1,1000);
+    array[i] = x;
+    Console.WriteLine(array[i]);
+}
