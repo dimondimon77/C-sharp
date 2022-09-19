5f51cea81d1ccdfab2ddfae401e1d7ea3e0b8a3d
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-19 21:20:54 +0300

    Полностью решена

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
-Console.WriteLine("Введите число num1 : = " );
-int num1 = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Введите число num2 : = " );
-int num2 = Convert.ToInt16(Console.ReadLine());
-Boolean briefly = true;
-briefly = num2%num1 == 0;
-if(briefly)
+Console.WriteLine("Введите число number : = " );
+int number = Convert.ToInt16(Console.ReadLine());
+int num1 = 7;
+int num2 = 20;
+Boolean comparison1 = true;
+Boolean comparison2 = true;
+comparison1 = number%num1 == 0;
+comparison2 = number%num2 == 0;
+
+if(comparison1&comparison2)
 {
-    Console.WriteLine("Второе число кратно первому");
+    Console.WriteLine("Успех");
 }
-if(!briefly)
-{
-    Console.WriteLine("Второе число не кратно первому");
-}
+//if(!briefly)
+//{
+    //Console.WriteLine("Второе число не кратно первому");
+//}
