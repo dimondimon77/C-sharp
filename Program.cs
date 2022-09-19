dd1db4a6de46acf3576f10d80d2d271afd760e16
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-19 20:27:45 +0300

    Рабочие моменты

--- Program.cs
+++ Program.cs
@@ -1,16 +1,3 @@
-Console.WriteLine("Введите число N : = " );
-int N = Convert.ToInt32(Console.ReadLine());
-int[] massiv = new int[N];
-int index = 0;
-int Value = 1;
-    for(; index < N;index++,Value++)
-{
-    massiv[index] = Value;
-    if ((massiv[index] % 2) == 0)
-    {
-    Console.WriteLine(massiv[index]);
-    }
-}
 
-
-
+int x = new Random(). Next(1,1000);
+Console.WriteLine(x);
