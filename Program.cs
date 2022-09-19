b59c60429f29c15c3bce16a743af5081bec6aa07
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-19 21:05:00 +0300

    Готовая

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-
-int x = new Random(). Next(1,1000);
-Console.WriteLine(x);
+Console.WriteLine("Введите число num1 : = " );
+int num1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите число num2 : = " );
+int num2 = Convert.ToInt16(Console.ReadLine());
+Boolean briefly = true;
+briefly = num2%num1 == 0;
+if(briefly)
+{
+    Console.WriteLine("Второе число кратно первому");
+}
+if(!briefly)
+{
+    Console.WriteLine("Второе число не кратно первому");
+}
