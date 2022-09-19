b1d51293825325ac9325be8f37e189d3f1bb9bef
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-19 20:47:17 +0300

    Почти

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-
-int x = new Random(). Next(1,1000);
-Console.WriteLine(x);
+Console.WriteLine("Введите число длину массива : = " );
+int lenght = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int random = new Random(). Next(1,100);
+int[] Array = new int[lenght];
+for (i=1;i<Array[lenght];i++)
+{
+    Array[i]= random;
+}
+Console.WriteLine(Array[i]);
