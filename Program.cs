de3a6278208d07e487f2c77b6f4348131ef4255b
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-20 16:08:35 +0300

    Определение длинны массива

--- Program.cs
+++ Program.cs
@@ -8,12 +8,43 @@
 
 Console.WriteLine("Введите число : = " );
 int num = Convert.ToInt32(Console.ReadLine());
-    if(num>=100)
-    {
-    int reduction = num%100%10;
-    Console.WriteLine(reduction);
+int lenght = 1;
+int temp = 0;
+int ten = 10;
+Boolean parity = true;
+Boolean stop = true;
+temp = num;
+stop = temp>10;
+parity = lenght%2 == 0;
+for(;stop;){
+for(;!parity;lenght++)
+{
+    parity = lenght%2 == 0;
+    temp = num/ten;
+    ten=ten*10;
+    stop = temp>10;
+    Console.WriteLine(temp);
+    Console.WriteLine(ten);
+    if(!stop){
+        Console.WriteLine(temp);
+        Console.WriteLine(lenght);
+        break;
     }
-    else
-    {
-        Console.WriteLine("Вы ввели двухзначное число, и третей цифры нет");
+}
+for(;parity;lenght++)
+{
+    parity = lenght%2 == 0;
+    temp = num/ten;
+    ten=ten*10;
+    stop = temp>10;
+   Console.WriteLine(temp);
+   Console.WriteLine(ten);
+       if(!stop){
+        Console.WriteLine(temp);
+        Console.WriteLine(lenght);
+        break;
+        }
     }
+}
+int[] massiv = new int[lenght];
+Console.WriteLine(massiv[7]);
