4a1fd6f762ec08929d606e8667b5cc9588a32faf
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-20 20:04:02 +0300

    Полностью рабочая

--- Program.cs
+++ Program.cs
@@ -8,12 +8,8 @@
 
 Console.WriteLine("Введите число : = " );
 int num = Convert.ToInt32(Console.ReadLine());
-int lenght = 1;
-int lenght2 = 0;
 int temp = 0;
-int temp2 =0;
 int ten = 10;
-int ten2 = 0;
 int count = 0;
 int countTarget = 3;
 int result = 0;
@@ -24,29 +20,25 @@ for(;temp>10;){
    Console.WriteLine(temp);
    Console.WriteLine(ten);
        if(temp<10){
-        lenght2 = lenght;
-        ten2=ten;
-        temp2 =temp;
-        Console.WriteLine(temp);
-        Console.WriteLine(temp2);
-        Console.WriteLine(lenght);
         break;
         }
     }
 while(count<countTarget)
 {
     count++;
-    temp2 = temp2*ten2;
-    ten2=ten2/10;
-    temp2 = num/ten2;
-    Console.WriteLine(ten2);
-    Console.WriteLine(temp2);
+    temp = temp*ten;
+    ten=ten/10;
+    temp = num/ten;
+    Console.WriteLine(ten);
+    Console.WriteLine(temp);
     Console.WriteLine(count);
 }
 if (count==3)
 {
-    result = temp2%100%10;
-    Console.WriteLine(result);
+    result = temp%100%10;
+    Console.Clear();
+    Console.WriteLine(num);
+    Console.WriteLine("=" + result);
 }
 
 
