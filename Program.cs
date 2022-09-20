bd44983320d1463b21b3ee8b97b4c514e2a76a49
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-20 04:53:25 +0300

    Решенная

--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-// 645 -> 5
-
-// 78 -> третьей цифры нет
-
-// 32679 -> 6
-
+// 6 -> да
+// 7 -> да
+// 1 -> нет
 Console.WriteLine("Введите число число : = " );
 int num = Convert.ToInt32(Console.ReadLine());
-    if(num>=100)
+Boolean saturday = true;
+Boolean sunday = true;
+saturday = num == 6;
+sunday = num == 7;
+    if(num>7)
     {
-    int reduction = num%100%10;
-    Console.WriteLine(reduction);
+    Console.WriteLine("Вы ввели число которое превышает количество дней в неделе, пожалуйста повторите попытку");
+    return;
     }
-    else
+    if(saturday|sunday)
     {
-        Console.WriteLine("Вы ввели двухзначное число, и третей цифры нет");
+        Console.WriteLine("Да");
     }
+        if(!(saturday|sunday))
+    {
+        Console.WriteLine("Нет");
+    }
+
 
 
 
