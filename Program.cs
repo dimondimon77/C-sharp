22bca5211b6c8610a92d3edad1ce6b06612b7fbf
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-20 04:12:27 +0300

    Решенная

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
 Console.WriteLine("Введите число");
-int lenght = Convert.ToInt32(Console.ReadLine());
-int[] array= new int[lenght];
-for (int i = 0;i<lenght;i++)
-{
-    int x = new Random(). Next(1,1000);
-    array[i] = x;
-    Console.WriteLine(array[i]);
-}
+int num = Convert.ToInt32(Console.ReadLine());
+int reduction = num%100;
+reduction = reduction%10;
+Console.WriteLine(reduction);
