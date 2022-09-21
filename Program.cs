52787af25ae2368512069d9c81613af6ddfa5c56
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-21 23:40:43 +0300

    Рабочая с рандомом и порядком чисел

--- Program.cs
+++ Program.cs
@@ -6,64 +6,59 @@
 
 // 32679 -> 6
 Console.Clear();
-int num = new Random().Next(1, 1000000);
+int num = new Random().Next(1, 10000000);
 int temp = 0;
+int ten = 10;
+int ten2 = 10;
 int count = 0;
 int countTarget = 3;
 int result = 0;
+int number = 0;
 temp = num;
 if (num<100)
 {
     Console.WriteLine("Третьей цифры нет");
 }
 else{
-void MassivLenght(int temp = 0,int ten = 10,int count = 0){
-    temp = num;
 for(;temp>10;){
-    count++;
     temp = num/ten;
     ten=ten*10;
-   Console.WriteLine(temp);
-   Console.WriteLine(ten);
+//    Console.WriteLine(temp);
+//    Console.WriteLine(ten);
        if(temp<10){
-        Console.WriteLine("count" + count);
-        return;
+        break;
         }
     }
-}
-MassivLenght(count);
+while(count<countTarget)
 {
-    Console.WriteLine("count"+ count);
-}
-}
-void Massiv(int[] massiv,int i = 0){
-    for (;i<count;i++)
-    {
-        massiv[i]= temp;
+    count++;
+    temp = temp*ten;
+    ten=ten/10;
+    temp = num/ten;
+    number = temp;
+    number = number%ten2;
+    // Console.WriteLine("=" +number);
+    if(count==1){
+        number = number%ten2;
+    Console.WriteLine("Первая цифра числа :=" +number);
     }
-
+    if(count==2){
+        number = number%ten2;
+    Console.WriteLine("Вторая цифра числа :=" +number);
+    }
+    if(count==3){
+        number = number%ten2;
+    Console.WriteLine("Третья цифра числа :=" +number);
+    }
+    // Console.WriteLine(ten);
+    // Console.WriteLine(temp);
+    // Console.WriteLine(count);
+    // Console.WriteLine(n);
 }
-int[] massiv = new int[count];
-Massiv(massiv);
+if (count==3)
 {
-    Console.WriteLine(massiv[2]);
+    result = temp%100%10;
+    Console.WriteLine(num);
+    Console.WriteLine("Третья цифра числа :=" + result);
 }
-
-
-// while(count<countTarget)
-// {
-//     count++;
-//     temp = temp*ten;
-//     ten=ten/10;
-//     temp = num/ten;
-//     Console.WriteLine(ten);
-//     Console.WriteLine(temp);
-//     Console.WriteLine(count);
-// }
-// if (count==3)
-// {
-//     result = temp%100%10;
-//     Console.WriteLine(num);
-//     Console.WriteLine("=" + result);
-// }
-// }
+}
