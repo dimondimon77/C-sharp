834946c15e194e44c45e163e08e86b846e84f5c8
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-22 21:12:19 +0300

    решенная через Boolean

--- Program.cs
+++ Program.cs
@@ -1,31 +1,24 @@
-// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// Задача 17 Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+//  и выдаёт номер четверти плоскости, в которой находится эта точка.
+// 1. = -x и y
+// 2. = x и Y
+// 3. = -x и -Y
+// 4. = x и -Y  
 
 Console.WriteLine("Введите число num1 : = " );
 Console.WriteLine("Введите число num2 : = " );
-Console.WriteLine("Введите число num3 : = " );
-Console.WriteLine("Введите число num4 : = " );
-Console.WriteLine("Введите число num5 : = " );
-Console.WriteLine("Введите число num6 : = " );
-int num1 = Convert.ToInt16(Console.ReadLine());
-int num2 = Convert.ToInt16(Console.ReadLine());
-int num3 = Convert.ToInt16(Console.ReadLine());
-int num4 = Convert.ToInt16(Console.ReadLine());
-int num5 = Convert.ToInt16(Console.ReadLine());
-int num6 = Convert.ToInt16(Console.ReadLine());
-if(num1>num2)
-Console.WriteLine("Максимальное число: =" + num1);
-else
-Console.WriteLine("Максимальное число: =" + num2);
-if(num3>num4)
-Console.WriteLine("Максимальное число: =" + num3);
-else
-Console.WriteLine("Максимальное число: =" + num4);
-if(num5>num6)
-Console.WriteLine("Максимальное число: =" + num5);
-else
-Console.WriteLine("Максимальное число: =" + num6);
+int x = Convert.ToInt16(Console.ReadLine());
+int y = Convert.ToInt16(Console.ReadLine());
+Boolean X=true;
+Boolean Y=true;
+X = x>0;
+Y = y>0;
+if(!X&Y)
+Console.WriteLine("Первая плоскость");
+if(X&Y)
+Console.WriteLine("Вторая плоскость");
+if(!X&!Y)
+Console.WriteLine("Третья плоскость");
+if(X&!Y)
+Console.WriteLine("Четвертая плоскость");
 
