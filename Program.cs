f0b6300db85b28e26e2c615cd983ff653b880793
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-23 23:37:32 +0300

    Решение для 3-ех точек

--- Program.cs
+++ Program.cs
@@ -1,25 +1,33 @@
-// Задача 18: Напишите программу, которая по заданному номеру четверти, 
-// показывает диапазон возможных координат точек в этой четверти (x и y).
-// 1. = -x и y
-// 2. = x и Y
-// 3. = -x и -Y
-// 4. = x и -Y  
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 Console.Clear();
-Console.WriteLine("Введите число num1 : = " );
-Console.WriteLine("Введите число num2 : = " );
-int x = Convert.ToInt16(Console.ReadLine());
-int y = Convert.ToInt16(Console.ReadLine());
-Boolean X=true;
-Boolean Y=true;
-X = x>0;
-Y = y>0;
-if(!X&Y)
-Console.WriteLine("Первая плоскость (Возможные координаты: -32768 : 32767)");
-if(X&Y)
-Console.WriteLine("Вторая плоскость (Возможные координаты: 0 : 32768)");
-if(!X&!Y)
-Console.WriteLine("Третья плоскость (Возможные координаты: -32768 : 0)");
-if(X&!Y)
-Console.WriteLine("Четвертая плоскость (Возможные координаты: -32768 : 32767)");
+Console.WriteLine("Введите число A1 : = " );
+Console.WriteLine("Введите число A2 : = " );
+Console.WriteLine("Введите число A3 : = " );
+Console.WriteLine("Введите число B1 : = " );
+Console.WriteLine("Введите число B2 : = " );
+Console.WriteLine("Введите число B3 : = " );
+double A1 = Convert.ToDouble(Console.ReadLine());
+double A2 = Convert.ToDouble(Console.ReadLine());
+double A3 = Convert.ToDouble(Console.ReadLine());
+double B1 = Convert.ToDouble(Console.ReadLine());
+double B2 = Convert.ToDouble(Console.ReadLine());
+double B3 = Convert.ToDouble(Console.ReadLine());
+double[] massivA = new double[] {A1, A2, A3};
+double[] massivB = new double[] {B1, B2, B3};
+double d = 0;
+double sum=0;
+for(int i = 0; i < massivA.Length;i++)
+{
+    d = massivB[i] - massivA[i];
+    d = Math.Pow(d,2);
+    sum = sum + d;
+}
+Console.WriteLine(Math.Sqrt(sum));
+
+
 
 
