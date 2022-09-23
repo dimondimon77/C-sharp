8ed010937a6e878feaf812a89f3d7ad0bdf09cce
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-23 21:44:10 +0300

    Полностью решена через массив

--- Program.cs
+++ Program.cs
@@ -6,18 +6,31 @@
 
 // 23432 -> да
 Console.Clear();
-// Console.WriteLine("Введите число : = " );
-// int num = Convert.ToInt32(Console.ReadLine());
-int num = new Random().Next(1,99999);
+Console.WriteLine("Введите число : = " );
+int num = Convert.ToInt32(Console.ReadLine());
+// int num = new Random().Next(1,99999);
 int[] massiv = new int[5];
 int ten = 1;
-for(int i = 0; i < massiv.Length;i++)
+int ten2 = 1;
+int i = 0;
+for(; i < massiv.Length;i++)
 {
     ten = ten * 10;
     massiv[i] = num%ten;
-    Console.WriteLine(massiv[i]);
-    Console.WriteLine(num);
+    massiv[i] = massiv[i]/ten2;
+    ten2 = ten2 * 10;
+    // Console.WriteLine(ten);
+    // Console.WriteLine(num);
+    // ten2 = ten2 * 10;
+    // massiv[i] = num/ten2;
+    // Console.WriteLine(massiv[i]);
+    // Console.WriteLine(num);
 }
-
-
-
+    if(massiv[0] == massiv[4] & massiv[1] == massiv[3])
+    {
+    Console.WriteLine("Число явялется палиндромом");
+    }
+    else
+    {
+    Console.WriteLine("Число не явялется палиндромом");  
+    }
