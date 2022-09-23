69db70e38e773ef52a69865e5111508ddc86d4c2
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-23 21:52:27 +0300

    Полностью решена

--- Program.cs
+++ Program.cs
@@ -1,36 +1,21 @@
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-// 14212 -> нет
-
-// 12821 -> да
-
-// 23432 -> да
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
 Console.Clear();
-Console.WriteLine("Введите число : = " );
-int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N : = " );
+int N = Convert.ToInt32(Console.ReadLine());
 // int num = new Random().Next(1,99999);
-int[] massiv = new int[5];
-int ten = 1;
-int ten2 = 1;
-int i = 0;
-for(; i < massiv.Length;i++)
+int[] massiv = new int[N];
+int i = 1;
+for(;i < massiv.Length;i++)
 {
-    ten = ten * 10;
-    massiv[i] = num%ten;
-    massiv[i] = massiv[i]/ten2;
-    ten2 = ten2 * 10;
+    int degree = Convert.ToInt32(Math.Pow(i, 3));
+    massiv[i] = degree;
     // Console.WriteLine(ten);
     // Console.WriteLine(num);
     // ten2 = ten2 * 10;
     // massiv[i] = num/ten2;
-    // Console.WriteLine(massiv[i]);
+    Console.WriteLine(massiv[i]);
     // Console.WriteLine(num);
 }
-    if(massiv[0] == massiv[4] & massiv[1] == massiv[3])
-    {
-    Console.WriteLine("Число явялется палиндромом");
-    }
-    else
-    {
-    Console.WriteLine("Число не явялется палиндромом");  
-    }
