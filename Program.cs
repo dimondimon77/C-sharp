380a08a5c1c44e3d29642862113979fe48e21dcf
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-24 02:54:21 +0300

    Полностью готовая

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 Console.Clear();
 Console.WriteLine("Введите число A1 : = " );
-Console.WriteLine("Введите число A2 : = " );
-Console.WriteLine("Введите число A3 : = " );
-Console.WriteLine("Введите число B1 : = " );
-Console.WriteLine("Введите число B2 : = " );
-Console.WriteLine("Введите число B3 : = " );
 double A1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число A2 : = " );
 double A2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число A3 : = " );
 double A3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B1 : = " );
 double B1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B2 : = " );
 double B2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B3 : = " );
 double B3 = Convert.ToDouble(Console.ReadLine());
 double[] massivA = new double[] {A1, A2, A3};
 double[] massivB = new double[] {B1, B2, B3};
