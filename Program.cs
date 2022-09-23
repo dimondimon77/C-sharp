9d657d19afeeaa19172c228043328ba08658a901
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-24 00:06:56 +0300

    Полностью готовая и убрано лишнее

--- Program.cs
+++ Program.cs
@@ -11,20 +11,11 @@ int num = Convert.ToInt32(Console.ReadLine());
 // int num = new Random().Next(1,99999);
 int[] massiv = new int[5];
 int ten = 1;
-int ten2 = 1;
 int i = 0;
 for(; i < massiv.Length;i++)
 {
     ten = ten * 10;
-    massiv[i] = num%ten;
-    massiv[i] = massiv[i]/ten2;
-    ten2 = ten2 * 10;
-    // Console.WriteLine(ten);
-    // Console.WriteLine(num);
-    // ten2 = ten2 * 10;
-    // massiv[i] = num/ten2;
-    // Console.WriteLine(massiv[i]);
-    // Console.WriteLine(num);
+    massiv[i] = (num%ten)/(ten/10);
 }
     if(massiv[0] == massiv[4] & massiv[1] == massiv[3])
     {
