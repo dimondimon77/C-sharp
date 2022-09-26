b43f1a41e130703bab2247f977cb235dcab3580f
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-26 21:08:04 +0300

    Готовая

--- Program.cs
+++ Program.cs
@@ -1,31 +1,16 @@
-// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
 
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
-
-Console.WriteLine("Введите число num1 : = " );
-Console.WriteLine("Введите число num2 : = " );
-Console.WriteLine("Введите число num3 : = " );
-Console.WriteLine("Введите число num4 : = " );
-Console.WriteLine("Введите число num5 : = " );
-Console.WriteLine("Введите число num6 : = " );
-int num1 = Convert.ToInt16(Console.ReadLine());
-int num2 = Convert.ToInt16(Console.ReadLine());
-int num3 = Convert.ToInt16(Console.ReadLine());
-int num4 = Convert.ToInt16(Console.ReadLine());
-int num5 = Convert.ToInt16(Console.ReadLine());
-int num6 = Convert.ToInt16(Console.ReadLine());
-if(num1>num2)
-Console.WriteLine("Максимальное число: =" + num1);
-else
-Console.WriteLine("Максимальное число: =" + num2);
-if(num3>num4)
-Console.WriteLine("Максимальное число: =" + num3);
-else
-Console.WriteLine("Максимальное число: =" + num4);
-if(num5>num6)
-Console.WriteLine("Максимальное число: =" + num5);
-else
-Console.WriteLine("Максимальное число: =" + num6);
+Console.WriteLine("Введите число num : = " );
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int ten = 1;
+for(int count = 0;temp>10;){
+    ten *=10;
+    temp = num/ten;
+    count++;
+    Console.WriteLine(count + 1);
+}
 
