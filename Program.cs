e189c26e724b2f65adbe5e323a41e5f3394096cf
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-29 00:48:05 +0300

    Решенная с двумя решениями

--- Program.cs
+++ Program.cs
@@ -7,28 +7,37 @@
 // 9012 -> 12
 
 Console.WriteLine("Введите число : = " );
-int num = Convert.ToInt32(Console.ReadLine());
-int temp = 0;
-int ten = 10;
-int count = 0;
-int i = -1;
-int number = 0;
-int result = 0;
-temp = num;
-while(temp>10){
-    temp = num/ten;
-    ten *= 10;
-    count++;
+String? num = Convert.ToString(Console.ReadLine());
+int sum = 0;
+for(int i = 0;i<num.Length;i++){
+    sum = num.Where(Char.IsDigit).Sum(c => c - '0'); // Складывает только цифры из стоки типа String 
 }
-while(i<count)
-{
-    i++;
-    temp *= ten;
-    ten /= 10;
-    temp = num/ten;
-    number = temp;
-    number %= 10;
-    result += number;
-}
-Console.WriteLine(result);
+Console.WriteLine(sum);
+
+// Решение через Математические операции (Only Integer)
+// Console.WriteLine("Введите число : = " );
+// int num = Convert.ToInt32(Console.ReadLine());
+// int temp = 0;
+// int ten = 10;
+// int count = 0;
+// int i = -1;
+// int number = 0;
+// int result = 0;
+// temp = num;
+// while(temp>10){
+//     temp = num/ten;
+//     ten *= 10;
+//     count++;
+// }
+// while(i<count)
+// {
+//     i++;
+//     temp *= ten;
+//     ten /= 10;
+//     temp = num/ten;
+//     number = temp;
+//     number %= 10;
+//     result += number;
+// }
+// Console.WriteLine(result);
 
