8671f3f11947773108024379534b2ef6143e23b5
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-28 19:34:37 +0300

    Решена

--- Program.cs
+++ Program.cs
@@ -3,16 +3,20 @@
 // 78 -> 2
 // 89126 -> 5
 
-Console.WriteLine("Введите число num : = " );
-int num = Convert.ToInt32(Console.ReadLine());
-int temp = num;
-int ten = 1;
-int count = 1;
-for(;temp>10;){
-    ten *= 10;
-    temp = num/ten;
-    count++;
-    Console.WriteLine(temp);
+Console.WriteLine("Введите число A : = " );
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B : = " );
+int B = Convert.ToInt32(Console.ReadLine());
+//Вот основное правильное решение
+Console.WriteLine("Результат = " + Math.Pow(A,B));
+//Цикл
+int temp = A;
+int i=1;
+int[] massiv = new int[B+1];
+for(; i < B;i++){
+temp *= A;
+massiv[i] = temp;
 }
-Console.WriteLine(count);
+Console.WriteLine("Результат = " + massiv[B-1]);
+
 
