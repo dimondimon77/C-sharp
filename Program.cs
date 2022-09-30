04e9e315931c609357cfad6db194eb230f93ae7b
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-30 23:36:04 +0300

    Решена

--- Program.cs
+++ Program.cs
@@ -3,22 +3,22 @@
 // [345, 897, 568, 234] -> 2
 
 int Length = new Random().Next(4,10);
-int[] massiv = new int[Length];
+int[] massiv = new int[Length]; // Создаем массив, длина от 5 до 9
 void RandomMassiv(int[] collect){
     int random = 0;
     for(int i = 0;i<collect.Length;i++){
-        random = new Random().Next(100,999);
+        random = new Random().Next(100,999); //Создаем функцию которая заполняет массив, рандомными числами
         collect[i] = random;
     }
 }
 RandomMassiv(massiv);
-string convert = String.Join("", massiv);
+string convert = String.Join("", massiv); // Тут переводим все цифры в одну строку
 int count = 0;
 for(int i = 0; i<convert.Length;i++){
-   char scan = convert[i]; 
+   char scan = convert[i]; // В этом цикле проверяем каждый символ на четность
    if(scan%2==0){
-    count++;
+    count++;  // Считаем количество четных цифр
    }
 }
-Console.WriteLine(convert);
-Console.WriteLine(count);
+Console.WriteLine(convert); // Выводим строку
+Console.WriteLine(count); // Выводим количество четных чисел
