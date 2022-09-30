1e21d079b4e0eaf2b0e128737b506f1ad9daa0bb
dimondimon77 <overlord_2_1997@mail.ru>
2022-09-30 23:31:15 +0300

    Решена

--- Program.cs
+++ Program.cs
@@ -7,29 +7,18 @@ int[] massiv = new int[Length];
 void RandomMassiv(int[] collect){
     int random = 0;
     for(int i = 0;i<collect.Length;i++){
-        random = new Random().Next(1,999);
+        random = new Random().Next(100,999);
         collect[i] = random;
     }
 }
-void ScanParity(int[] scan){
-    int count = 0;
-    for(int i = 0;i<scan.Length;i++){
-        int sum = num.Where(Char.IsDigit).Sum(c => c - '0');
-    }
-}
-void MassivPrint(int[] print){
-        for(int i = 0;i<print.Length;i++){
-        Console.WriteLine(print[i]);
-    }
-}
 RandomMassiv(massiv);
-MassivPrint(massiv);
-// Вариант без функций
-// int random1 = new Random().Next(1,11);
-// for(int i = 0;i<massiv.Length;i++){
-//         random1 = new Random().Next(1,11);
-//         massiv[i] = random1;
-//         Console.WriteLine(massiv[i]);
-//     }
-
-
+string convert = String.Join("", massiv);
+int count = 0;
+for(int i = 0; i<convert.Length;i++){
+   char scan = convert[i]; 
+   if(scan%2==0){
+    count++;
+   }
+}
+Console.WriteLine(convert);
+Console.WriteLine(count);
