256a0b4fa11b8ac8fef86791434a3c5566225e0c
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-03 13:04:34 +0300

    Решена

--- Program.cs
+++ Program.cs
@@ -32,25 +32,3 @@ RandomMassiv(massiv);
 PrintMassiv(massiv);
 NonParitySum(massiv);
 Console.WriteLine("Сумма нечетных чисел = " + sum);
-// // BOUNUS
-// // Программа которая выводит количество четных цифр во всем массиве
-// int Length = new Random().Next(4,10);
-// int[] massiv = new int[Length]; // Создаем массив, длина от 5 до 9
-// void RandomMassiv(int[] collect){ //Создаем функцию которая заполняет массив, рандомными числами
-//     int random = 0;
-//     for(int i = 0;i<collect.Length;i++){
-//         random = new Random().Next(100,999);
-//         collect[i] = random;
-//     }
-// }
-// RandomMassiv(massiv); // Используем функцию для созданного массива
-// string convert = String.Join("", massiv); // Тут переводим все цифры в одну строку
-// int count = 0;
-// for(int i = 0; i<convert.Length;i++){
-//    char scan = convert[i]; // В этом цикле проверяем каждый символ на четность
-//    if(scan%2==0){
-//     count++;  // Считаем количество четных цифр
-//    }
-// }
-// Console.WriteLine(convert); // Выводим строку
-// Console.WriteLine(count); // Выводим количество четных цифр
