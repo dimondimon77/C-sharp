ef89559c0ff66e5ff64ff5934cfb14b063b4f13b
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-03 21:25:29 +0300

    Начало

--- Program.cs
+++ Program.cs
@@ -1,17 +1,8 @@
+// **Задача 42:** Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
-// **Задача 40:** Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник 
-// с сторонами такой длины.
+// 45 -> 101101
 
-// ❗ **Теорема о неравенстве треугольника:** каждая сторона треугольника меньше суммы двух других сторон.
+// 3 -> 11
 
-Console.WriteLine("Введите число num1 : = " );
-Console.WriteLine("Введите число num2 : = " );
-Console.WriteLine("Введите число num3 : = " );
-int num1 = Convert.ToInt16(Console.ReadLine());
-int num2 = Convert.ToInt16(Console.ReadLine());
-int num3 = Convert.ToInt16(Console.ReadLine());;
-if(num1<=num2+num3){Console.WriteLine("Треугольник может существовать");}
-else if(num2<=num1+num3){Console.WriteLine("Треугольник может существовать");}
-else if(num3<=num1+num2){Console.WriteLine("Треугольник может существовать");}
-else if(num1>num2+num3 & num2>num1+num3 & num3>num1+num2)
-{Console.WriteLine("Треугольник не может существовать");}
+// 2 -> 10
+
