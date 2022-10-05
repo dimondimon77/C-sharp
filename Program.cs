2b91d9ad920bfdf9dfbcd00f994108960ad35ae7
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-05 04:22:55 +0300

    Решена

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
-string[] daysweek = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
-int index = 0;
-Console.WriteLine("Введите число");
-index = Convert.ToInt16(Console.ReadLine());
-index = index - 1;
-Console.WriteLine(daysweek[index]);
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите число 1 : = " );
+int num1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите число 2 : = " );
+int num2 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите число 3 : = " );
+int num3 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите число 4 : = " );
+int num4 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите число 5 : = " );
+int num5 = Convert.ToInt16(Console.ReadLine());
+int[] massiv = new int[]{num1,num2,num3,num4,num5};
+int M = 0;
+for(int i = 0;i<massiv.Length;i++){
+    if(massiv[i]>0){
+        M++;
+    }
+}
+Console.WriteLine("Количество чисел больше нуля = " + M);
