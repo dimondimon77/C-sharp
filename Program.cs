14834e87c79609b832bffcec8d4307ecaafb896f
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-05 05:19:27 +0300

    Решена

--- Program.cs
+++ Program.cs
@@ -3,17 +3,17 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Введите число 1 : = " );
+Console.WriteLine("Введите число b1 : = " );
 int b1 = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Введите число 2 : = " );
+Console.WriteLine("Введите число k1 : = " );
 int k1 = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Введите число 3 : = " );
+Console.WriteLine("Введите число b2 : = " );
 int b2 = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Введите число 4 : = " );
+Console.WriteLine("Введите число k2 : = " );
 int k2 = Convert.ToInt16(Console.ReadLine());
 double x = -10;
 for(double y1 = 0, y2 = 0; x<10 ;){
-    x += 0.5;
+    x += 0.1;
     y1=k1*x+b1;
     y2=k2*x+b2;
     if(y1==y2){
