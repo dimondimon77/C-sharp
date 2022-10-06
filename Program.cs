8b36d76486654da93069081787cce329a0123a86
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-06 21:15:06 +0300

    Решена

--- Program.cs
+++ Program.cs
@@ -1,27 +1,25 @@
-// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
-// и замените эти элементы на их квадраты.
-// Например, изначально массив
-// выглядел вот так:
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
 
 Console.Clear();
 int Lenght0 = new Random().Next(3,10);
 int Lenght1 = new Random().Next(3,10);
 int[,] massiv = new int[Lenght0, Lenght1];
+int sum = 0;
 void RandomMassiv(int[,] array)
 {
-    for (int i = 0; i < Lenght0; i++)
+    int diagonal = 0;
+    for (int i = 0; i < Lenght0; i++,diagonal++)
     {
         for (int j = 0; j < Lenght1; j++)
         {
             array[i, j] = new Random().Next(1, 10);
-            if(i % 2 == 0 && j % 2 == 0)
-            {
-            array[i, j] = array[i, j] * array[i, j];
-            }
         }
+        sum += array[diagonal,diagonal];
     }
 }
 void PrintMassiv(int[,] print)
@@ -37,4 +35,5 @@ void PrintMassiv(int[,] print)
 }
 RandomMassiv(massiv);
 PrintMassiv(massiv);
+Console.Write(sum);
 
