5b6ab37cb8be4ccfa19412ca25254adeb1544f55
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-09 18:09:21 +0300

    Готовая

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 Console.Clear();
-int Lenght0 = new Random().Next(3,5);
-int Lenght1 = new Random().Next(3,5);
+int Lenght0 = new Random().Next(2,5);
+int Lenght1 = new Random().Next(2,5);
 int[,] massiv = new int[Lenght0, Lenght1];
 void RandomMassiv(int[,] array)
 {
@@ -22,14 +22,14 @@ void RandomMassiv(int[,] array)
 }
 void AverageSumColumn(int[,] avarageSum)
 {
-    int Sum = 0;
-    for(int i = 0; i < avarageSum.GetLength(1);i++)
+    double Sum = 0;
+    for(int i = 0; i < avarageSum.GetLength(0);i++)
     {
         for (int j = 0; j < avarageSum.GetLength(1); j++)
         {
             Sum += avarageSum[j,i];
         }       
-        Sum = Sum/3; 
+        Sum = Sum/avarageSum.GetLength(1); 
         Console.WriteLine($"Ср. Арифм. столбец ({i+1}) = {Sum}");
         Sum = 0;
     }
