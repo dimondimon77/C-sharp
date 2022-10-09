9fc19ca3472189b984ee4f87aecff5344273611a
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-09 18:11:37 +0300

    Готовая

--- Program.cs
+++ Program.cs
@@ -12,9 +12,9 @@ Console.Clear();
 double[,] massiv = new double[3, 4];
 void RandomMassiv(double[,] array)
 {
-    for (int i = 0; i < massiv.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < massiv.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             array[i, j] = new Random().Next(-10,10) + new Random().NextDouble();
             array[i, j] = Math.Round(array[i, j],1);
