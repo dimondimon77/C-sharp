fc2bc902e34efabea01064823d44d3f210563612
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-12 21:53:31 +0300

    Середина решения

--- Program.cs
+++ Program.cs
@@ -7,35 +7,41 @@
 
 Console.Clear();
 int[,] massiv = new int[4, 4];
-void FillArray(int[,] fill,int x,int y){
-    if(x == 0 & y == 4){
-    for (int i = 0; i < y; i++)
-    {
-        fill[x,i]= new Random().Next(10,100);
-    }   
-    }  
-    if(x == 4 & y == 0){
-    for (int i = 0; i < x; i++)
-    {
-        fill[i,fill.GetLength(1)-1]= new Random().Next(10,100);
-    }   
-    } 
-    if (x==4 & y==4){
-    for (int i = 0; i < y; i++)
-    {
-        fill[x-1,i]= new Random().Next(10,100);
-    }  
-    }
-    
-    // for (int i = 0; i < fill.GetLength(0); i++)
-    // {
-    // for (int j = 0; j < fill.GetLength(1); j++)
-    // {
-    //     fill[i,j]= new Random().Next(10,100);
-    // }   
-    // }
-    }
-    
+int count = 0;
+void FillArray(int[,] fill, int x, int y, int fillX, int fillY)
+{
+            if (fillX > 0)
+            {
+                for (int i = 0; i < fillX; i++,count ++)
+                {
+                    fill[i, y - 1] = count;
+                }
+            }
+            if (fillY > 0)
+            {
+                for (int i = 0; i < fillY; i++)
+                {
+                    count ++;
+                    fill[x - 1, i] = count ;
+                }
+            }
+            if (fillX < 0)
+            {
+                for (int i = 0; i > fillX; i++,count ++)
+                {
+                    fill[i, y-1] = count;
+                }
+            }
+            if (fillY < 0)
+            {
+                for (int i = 0; i < fillY; i++)
+                {
+                    count ++;
+                    fill[x - 1, i] = count ;
+                }
+            }
+}
+
 void PrintMassiv(int[,] print)
 {
     for (int i = 0; i < print.GetLength(0); i++)
@@ -48,10 +54,10 @@ void PrintMassiv(int[,] print)
     }
     Console.WriteLine();
 }
-FillArray(massiv,0,4);
-FillArray(massiv,4,0);
-FillArray(massiv,4,4);
-FillArray(massiv,4,0);
+FillArray(massiv, 1, 0, 0, 4);
+FillArray(massiv, 0, 4, 4, 0);
+FillArray(massiv, 4, 1, 0, 4);
 PrintMassiv(massiv);
+Console.WriteLine(count);
 
 
