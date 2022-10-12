25d3ab14e404e021d4e3cfc5548b1c681439270c
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-12 17:00:20 +0300

    Решена

--- Program.cs
+++ Program.cs
@@ -7,10 +7,11 @@
 // 15 18
 
 Console.Clear();
-Console.Clear();
 int Lenght0 = new Random().Next(3, 10);
 int Lenght1 = new Random().Next(3, 10);
-int[,] massiv = new int[Lenght0, Lenght1];
+int[,] massiv1 = new int[Lenght0, Lenght1];
+int[,] massiv2 = new int[Lenght0, Lenght1];
+ int[,] matrixMultiply = new int[massiv1.GetLength(0),massiv1.GetLength(1)];
 void RandomMassiv(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -21,6 +22,16 @@ void RandomMassiv(int[,] array)
         }
     }
 }
+void MultiplyMatrix(int[,] array1, int [,] array2)
+{
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+        matrixMultiply[i,j] = array1[i,j] * array2[i,j];
+        }
+    }
+    }
 void PrintMassiv(int[,] print)
 {
     for (int i = 0; i < print.GetLength(0); i++)
@@ -33,9 +44,11 @@ void PrintMassiv(int[,] print)
     }
     Console.WriteLine();
 }
-RandomMassiv(massiv);
-PrintMassiv(massiv);
-SortedMassiv(massiv);
-PrintMassiv(massiv);
+RandomMassiv(massiv1);
+RandomMassiv(massiv2);
+MultiplyMatrix(massiv1,massiv2);
+PrintMassiv(massiv1);
+PrintMassiv(massiv2);
+PrintMassiv(matrixMultiply);
 
 
