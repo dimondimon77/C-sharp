6871ad8ec57a76bf7dd12cb469b8021eae3c1393
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-12 12:31:50 +0300

    Начало

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
-// и замените эти элементы на их квадраты.
-// Например, изначально массив
-// выглядел вот так:
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
 
 Console.Clear();
-int Lenght0 = new Random().Next(3,10);
-int Lenght1 = new Random().Next(3,10);
+int Lenght0 = new Random().Next(3, 10);
+int Lenght1 = new Random().Next(3, 10);
 int[,] massiv = new int[Lenght0, Lenght1];
 void RandomMassiv(int[,] array)
 {
@@ -17,12 +19,23 @@ void RandomMassiv(int[,] array)
         for (int j = 0; j < Lenght1; j++)
         {
             array[i, j] = new Random().Next(1, 10);
-            if(i % 2 == 0 && j % 2 == 0)
-            {
-            array[i, j] = array[i, j] * array[i, j];
-            }
         }
     }
+}
+void SortedMassiv(int[,] sort)
+{
+    for (int i = 0; i < sort.GetLength(0); i++)
+    {
+        for (int j = 0,temp = 0; j < sort.GetLength(1); j++)
+        {
+            if (sort[i,j] < sort[i,j + 1]) {
+                temp = sort[i,j];
+                sort[i,j] = sort[i,j + 1];
+                sort[i,j + 1] = temp;
+            if(j==sort.GetLength(1)-2) return;
+        }
+    }
+}
 }
 void PrintMassiv(int[,] print)
 {
@@ -34,7 +47,11 @@ void PrintMassiv(int[,] print)
         }
         Console.WriteLine();
     }
+    Console.WriteLine();
 }
 RandomMassiv(massiv);
 PrintMassiv(massiv);
+SortedMassiv(massiv);
+PrintMassiv(massiv);
+
 
