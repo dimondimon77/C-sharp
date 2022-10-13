1e45aa9d62bf8ec3660d5c1eb73331f54dc614f2
dimondimon77 <overlord_2_1997@mail.ru>
2022-10-13 20:13:42 +0300

    начало второго решения

--- Program.cs
+++ Program.cs
@@ -4,58 +4,64 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
-
 Console.Clear();
-int[,] massiv = new int[,] {{1,1,1,1,1,1},
-                            {1,0,0,0,0,1},
-                            {1,0,0,0,0,1},
-                            {1,0,0,0,0,1},
-                            {1,0,0,0,0,1},
-                            {1,1,1,1,1,1},};
-int x = 0;
-void Fill(int row, int col)
-{
-    x++;
-    if (x == 12)
-    {
-        Fill(row - 1, col);
-        massiv[2, 1] = 12;
-    }
-    if (massiv[row, col] == 0)
-    {
-        massiv[row, col] = x;
-        Fill(row, col + 1);
-        if (massiv[row, col] != 0)
-        {
-            x--;
-            Fill(row + 1, col);
-        }
-        if (massiv[row, col] != 0)
-        {
-            x--;
-            Fill(row, col - 1);
-        }
-        if (massiv[row, col] != 0)
-        {
-            x--;
-            Fill(row - 1, col);
-        }
-    }
+int[,] massiv = new int[4,4];
+void FillRight()
+for(int i= 0;i<massiv.GetLength(1);i++){
+    massiv[0,i] = i;
+    Console.WriteLine(massiv[0,i]);
 }
+// Console.Clear();
+// int[,] massiv = new int[,] {{1,1,1,1,1,1},
+//                             {1,0,0,0,0,1},
+//                             {1,0,0,0,0,1},
+//                             {1,0,0,0,0,1},
+//                             {1,0,0,0,0,1},
+//                             {1,1,1,1,1,1},};
+// int x = 0;
+// void Fill(int row, int col)
+// {
+//     x++;
+//     if (x == 12)
+//     {
+//         Fill(row - 1, col);
+//         massiv[2, 1] = 12;
+//     }
+//     if (massiv[row, col] == 0)
+//     {
+//         massiv[row, col] = x;
+//         Fill(row, col + 1);
+//         if (massiv[row, col] != 0)
+//         {
+//             x--;
+//             Fill(row + 1, col);
+//         }
+//         if (massiv[row, col] != 0)
+//         {
+//             x--;
+//             Fill(row, col - 1);
+//         }
+//         if (massiv[row, col] != 0)
+//         {
+//             x--;
+//             Fill(row - 1, col);
+//         }
+//     }
+// }
 
-void PrintMassiv(int[,] print)
-{
-    for (int i = 1; i < print.GetLength(0) - 1; i++)
-    {
-        for (int j = 1; j < print.GetLength(1) - 1; j++)
-        {
-            Console.Write($"{print[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
-Fill(1, 1);
-PrintMassiv(massiv);
+// void PrintMassiv(int[,] print)
+// {
+//     for (int i = 1; i < print.GetLength(0) - 1; i++)
+//     {
+//         for (int j = 1; j < print.GetLength(1) - 1; j++)
+//         {
+//             Console.Write($"{print[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+// Fill(1, 1);
+// PrintMassiv(massiv);
 
 
